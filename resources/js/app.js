e49afe4ad8e196ea444523d6947cdd75609ee78c
Julian Atkin <jatkindev@gmail.com>
2022-04-12 11:50:50 -0400

    Add PrimeVue to Vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import PrimeVue from 'primevue/config';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,6 +13,7 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .use(PrimeVue)
             .mixin({ methods: { route } })
             .mount(el);
     },
