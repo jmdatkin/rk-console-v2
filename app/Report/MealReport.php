0d7a43b976f71a8a24eea3669de627cee6533647
Julian Atkin <jatkindev@gmail.com>
2022-05-17 22:03:54 -0400

    Basic MealReport data collection

--- app/Report/MealReport.php
+++ app/Report/MealReport.php
@@ -2,29 +2,70 @@
 
 namespace App\Report;
 
-use App\Repository\DriverRepositoryInterface;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\DB;
+use App\Models\Route;
+use App\Repository\RecipientRepositoryInterface;
+use App\Repository\RouteRepositoryInterface;
+use Illuminate\Support\Facades\Log;
 
 class MealReport extends BaseReport {
 
-    public function __construct(DriverRepositoryInterface $repository)
+    public function __construct(RecipientRepositoryInterface $repository, RouteRepositoryInterface $routeRepository)
     {
         $this->repository = $repository;
+        $this->routeRepository = $routeRepository;
     }
 
     public function data() {
 
     }
 
-    public function driver($driver_id) {
-        try {
-        return $this->repository->find($driver_id)->routes->map(function($route) {
-            return $route->recipients->load(['person']);
+    public function meals($weekday) {
+
+        return $this->routeRepository->all()
+        ->filter(function($route) use ($weekday) {
+            return !$route->recipients()->wherePivot('weekday',$weekday)->get()->isEmpty();
+        })
+        ->values()->map(function($route) use ($weekday) {
+            $aggNumMeals = $route->recipients->reduce(function($carry, $item) {
+                return $carry + $item->numMeals;
+            }, 0);
+            return [
+                'routeName' => $route->name,
+                'aggNumMeals' => $aggNumMeals
+            ];
+            // return $route;
         });
-        } catch (ModelNotFoundException $e) {
-            return collect();
-        }
+
+
+
+        // return $this->repository->all()
+        // ->filter(function($recipient) use ($weekday) {
+        //     return !$recipient->routes()->wherePivot('weekday', $weekday)->get()->isEmpty();
+        // })
+        // ->reduce(function($recipient) {
+        //     return $carry + $recipient->numMeals;  
+        // })
+
+        // return $this->routeRepository->all()
+        // ->load('recipients')
+        // ->filter(function($r) use ($weekday) {
+        //     return $r->recipients
+        // })
+
+        // return Route::with(['recipients' => function($query) use ($weekday) {
+        //     return $query->wherePivot('weekday', $weekday);
+        // }])->get();
+
+
+        // ->map(function($route) use ($weekday) {
+        //    $aggNumMeals = $route->recipients->reduce(function($carry, $item) {
+        //        return $carry + $item->numMeals;
+        //    });
+        //    return $route->union(['aggNumMeals' => $aggNumMeals]);
+        // });
+        // ->map(function($r) use ($weekday) {
+        //     // return $r->union($r->recipients()->wherePivot('weekday',$weekday)->get());
+        // })->filter(function($c) { return !$c->isEmpty(); });
     }
 
 }
