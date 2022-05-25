137ce7cc3608dea11d56ca66b60a923c2e569d13
Julian Atkin <jatkindev@gmail.com>
2022-05-25 17:34:14 -0400

    Created migration for driver alternate routes

---
+++ database/migrations/2022_05_25_213329_create_driver_route_alt_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('driver_route_alt', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('driver_id');
+            $table->unsignedBigInteger('route_id');
+            $table->foreign('driver_id')->references('id')->on('drivers');
+            $table->foreign('route_id')->references('id')->on('routes');
+            $table->unique(['driver_id','route_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver_route_alt');
+    }
+};
