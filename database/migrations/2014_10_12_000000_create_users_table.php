bea6a45ef6e2b6dfda251c109ae5313c761ee30a
Julian Atkin <jatkindev@gmail.com>
2022-07-28 04:30:30 -0400

    Add settings column to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             // $table->foreignId('person_id')->constrained('people');
             $table->unsignedBigInteger('person_id');
             $table->foreign('person_id')->references('id')->on('people');
+            $table->json('settings');
             // $table->string('name');
             // $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
