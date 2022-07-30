4addb80244db99c1e564b5b259ed9bcdbcd44035
Julian Atkin <jatkindev@gmail.com>
2022-07-30 01:03:32 -0400

    Start testing separating stages in report flow into own Vue pages

--- resources/js/Pages/Admin/Reports/OutreachReportDateSelect.vue
+++ resources/js/Pages/Admin/Reports/OutreachReportDateSelect.vue
@@ -2,9 +2,11 @@
 import ReportLayout from '@/Layouts/ReportLayout';
 import DateSelect from '../../../Components/DateSelect.vue';
 import OutreachReportTable from '../../../Components/OutreachReport/OutreachReportTable.vue';
+import { useBreadcrumb } from '../../../hooks';
+import ReportWeekView from '../../../Components/ReportWeekView.vue';
+import OutreachReportDateLayer from '../../../Components/OutreachReport/OutreachReportDateLayer.vue';
 import ReportWeekDataLayer from '../../../Components/ReportWeekDataLayer.vue';
-
-
+// import route from '../../../../../vendor/tightenco/ziggy/src/js';
 
 const getDataFunction = function(date) {
     return axios.create({
@@ -21,15 +23,7 @@ const getDataFunction = function(date) {
             Outreach Report
         </template>
         <template #report>
-
             <DateSelect>
-                <template v-slot="{ date, openDateSelect }">
-                    <!-- <OutreachReportDateLayer :date="date" :openDateSelect="openDateSelect"></OutreachReportDateLayer> -->
-                    <ReportWeekDataLayer :reportComponent="OutreachReportTable" :date="date" :openDateSelect="openDateSelect"
-                    :getDataFunction="getDataFunction"
-                    :requestURL="route('report.outreach.data')"
-                    ></ReportWeekDataLayer>
-                </template>
             </DateSelect>
         </template>
     </ReportLayout>
