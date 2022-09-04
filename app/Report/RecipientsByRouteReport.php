12e0c302a6ebd09906dd4b13c0eafa08373ebf2e
Julian Atkin <jatkindev@gmail.com>
2022-09-04 17:43:14 -0400

    Add has_driver_associated to recipientrouteview

--- app/Report/RecipientsByRouteReport.php
+++ app/Report/RecipientsByRouteReport.php
@@ -2,13 +2,14 @@
 
 namespace App\Report;
 
+use App\Models\DriverRoute;
 use Facades\App\Facade\DateAdapter;
 use App\Models\Route;
 use App\Repository\RouteRepositoryInterface;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class RecipientsByRouteReport implements ReportInterface
+class RecipientsByRouteReport
 {
 
     public function __construct(RouteRepositoryInterface $repository)
@@ -22,20 +23,43 @@ class RecipientsByRouteReport implements ReportInterface
      * @param array $input
      * @return \Illuminate\Support\Collection
      */
-    public function data($input)
+    public function data($date)
     {
-        $date = $input['date'];
-        return Route::with(['recipients' => function ($query) use ($date) {
-            // return $query->where('weekday', $date->lowercaseDayName());
-            return $query->where('weekday', $date->dayOfWeek);
-        }])
+        $weekday = $date->dayOfWeek;
+
+        return Route::with([
+            'recipients' => function ($query) use ($date) {
+                return $query->where('weekday', $date->dayOfWeek);
+            },
+        ])
             ->get()
-            ->flatMap(function ($route) {
-                return $route->recipients->map(function ($recipient) use ($route) {
-                    return collect($recipient->toArray())->union([
-                        'routeName' => $route->name
-                    ]);
+            ->flatMap(function ($route) use ($weekday) {
+                return $route->recipients->map(function ($recipient) use ($route, $weekday) {
+                    // return collect($recipient->toArray())->union([
+                    //     'routeName' => $route->name,
+                    // ]);
+                    $driver_associated = DriverRoute::where(['weekday' => $weekday, 'route_id' => $route->id])->exists();
+                    return array_merge($recipient->toArray(),
+                        [
+                            'routeName' => $route->name,
+                            'has_driver_associated' => $driver_associated
+                        ]
+                        );
                 });
             })->values();
     }
+    //     $date = $input['date'];
+    //     return Route::with(['recipients' => function ($query) use ($date) {
+    //         // return $query->where('weekday', $date->lowercaseDayName());
+    //         return $query->where('weekday', $date->dayOfWeek);
+    //     }])
+    //         ->get()
+    //         ->flatMap(function ($route) {
+    //             return $route->recipients->map(function ($recipient) use ($route) {
+    //                 return collect($recipient->toArray())->union([
+    //                     'routeName' => $route->name
+    //                 ]);
+    //             });
+    //         })->values();
+    // }
 }
