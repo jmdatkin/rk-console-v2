ee7a41b957993d6f926646329e97a7978bc11771
Julian Atkin <jatkindev@gmail.com>
2022-09-08 21:56:52 -0400

    Add id column to pending jobs table

--- database/migrations/2022_07_01_171740_create_pending_jobs_table.php
+++ database/migrations/2022_07_01_171740_create_pending_jobs_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('pending_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('job_name');
-            $table->json('payload');
+            $table->unsignedBigInteger('resource_id')->nullable();
+            $table->json('payload')->nullable();
             $table->timestamp('committed_at')->nullable()->default(null);
             $table->timestamps();
         });
